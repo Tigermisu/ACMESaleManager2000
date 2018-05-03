904c9395c7ecdac01de6c3986addb0d062f2b4e8
Unknown <tigermisu@outlook.com>
2018-05-02 20:11:22 -0600

    low stock email warning

--- ACMESaleManager2000/DomainServices/SaleOrderService.cs
+++ ACMESaleManager2000/DomainServices/SaleOrderService.cs
@@ -1,5 +1,8 @@
 ﻿using ACMESaleManager2000.DataRepositories;
 using ACMESaleManager2000.DomainObjects;
+using ACMESaleManager2000.Models;
+using ACMESaleManager2000.Services;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +13,18 @@ namespace ACMESaleManager2000.DomainServices
     public class SaleOrderService : Service<SaleOrder>, ISaleOrderService
     {
         protected readonly IItemRepository _itemRepository;
+        protected readonly IEmailSender _emailSender;
+        protected readonly UserManager<ApplicationUser> _userManager;
+        protected const int INVENTORY_EMAIL_THRESHOLD = 5;
 
-        public SaleOrderService(IRepository<SaleOrder> repository, IItemRepository itemRepository) : base(repository)
+        public SaleOrderService(IRepository<SaleOrder> repository, 
+            IItemRepository itemRepository, 
+            IEmailSender emailSender,
+            UserManager<ApplicationUser> userManager) : base(repository)
         {
             _itemRepository = itemRepository ?? throw new ArgumentNullException(nameof(itemRepository));
+            _emailSender = emailSender ?? throw new ArgumentNullException(nameof(emailSender));
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
         }
 
         public void SubtractFromItemInventory(int itemId, int quantity)
@@ -31,7 +42,23 @@ namespace ACMESaleManager2000.DomainServices
                 if (itemSale.SoldQuantity > item.QuantityAvailable) {
                     return false;
                 }
+
+                if (item.QuantityAvailable - itemSale.SoldQuantity < INVENTORY_EMAIL_THRESHOLD) {
+                    SendLowStockWarningEmail(item).Wait();
+                }
             }
+
+            return true;
+        }
+
+        private async Task<bool> SendLowStockWarningEmail(Item item) {
+            string message = $"{item.Name}'s stock has gone below the threshold of {INVENTORY_EMAIL_THRESHOLD}!";
+            var adminEmails = await _userManager.GetUsersInRoleAsync("Admin");
+            var supervisorEmails = await _userManager.GetUsersInRoleAsync("Supervisor");
+
+            adminEmails.Union(supervisorEmails).ToList().ForEach(user => {
+                _emailSender.SendEmailAsync(user.Email, "Low stock in item!", message).Wait();
+            });
 
             return true;
         }
